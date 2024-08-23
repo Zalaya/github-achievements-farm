982512c2eea4088ccbc405fdf7aaba74abf2fbed
zalaya <enol.corzal@gmail.com>
2024-08-23 20:03:06 +0200

    refactor(setup): update the logs on the setup and setdown scripts, now they give more information to the user

--- set-down.sh
+++ set-down.sh
@@ -1,17 +1,26 @@
 #! /bin/bash
 
 if [ -f ".env" ]; then
+    echo "Setting up environment variables..."
     export $(grep -v '^#' .env | xargs)
+    echo "Environment variables set up successfully!"
 else
+    echo "Environment variables not found!"
     exit 1
 fi
 
 cd ..
 
 if [ -d "$REPOSITORY_NAME" ]; then
+    echo "Removing existing directory..."
     rm -rf "$REPOSITORY_NAME"
+else
+    echo "Directory not found!"
 fi
 
 if gh repo view "$REPOSITORY_NAME" &> /dev/null; then
+    echo "Deleting existing repository..."
     gh repo delete "$REPOSITORY_NAME" --yes
+else
+    echo "Repository not found!"
 fi
